182c1d513a747f396b0ba39f4a9549b909c330ca
Tomohiro (Tomo) SHIOYA <shioyang4321@gmail.com>
2020-05-29 03:42:32 +0900

    feat: update README and refactor (#3)

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,29 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 async function run() {
-  try {
-    const token = core.getInput("repo-token", { required: true });
+  const token = core.getInput("repo-token", { required: true });
 
-    const owner = github.context.repo.owner;
-    const repo = github.context.repo.repo;
-    const commit_sha = github.context.sha
-    console.log(commit_sha);
+  const owner = github.context.repo.owner;
+  const repo = github.context.repo.repo;
+  const commit_sha = github.context.sha;
+  console.log(commit_sha);
 
-    const client = new github.GitHub(token);
-    const response = await client.repos.listPullRequestsAssociatedWithCommit({
-      owner,
-      repo,
-      commit_sha,
-    });
+  const client = new github.GitHub(token);
+  const response = await client.repos.listPullRequestsAssociatedWithCommit({
+    owner,
+    repo,
+    commit_sha,
+  });
 
-    const pull_request =
-      response.data.length > 0 ? JSON.stringify(response.data[0]) : "";
-    console.log(pull_request);
-    core.setOutput("pull_request", pull_request);
-  } catch (error) {
-    core.setFailed(error.message);
+  const pr = response.data.length > 0 && response.data[0];
+  if (pr) {
+    console.log(pr);
   }
+
+  const pull_request = pr ? JSON.stringify(pr) : "";
+  core.setOutput("pull_request", pull_request);
 }
 
-run();
+run().catch((err) => {
+  core.setFailed(err.message);
+});
